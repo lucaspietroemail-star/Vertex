1963598a5f8af7c728b4daeb1578b978479f3a0a
lucaspietroemail-star <lucaspietro.email@gmail.com>
2026-06-29 04:01:41 -0300

    Add minimal Android Seed application structure

---
+++ seed/src/main/kotlin/vertex/seed/MainActivity.kt
@@ -0,0 +1,13 @@
+package vertex.seed
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Temporary launcher activity for Seed 0.1.
+ */
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
